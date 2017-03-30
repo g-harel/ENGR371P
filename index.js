76ca0edd3c35efe65d185a1afdadcd7eb3b56ed2
Gabriel <pabombs@gmail.com>
2017-03-30 00:09:38 -0400

    added some function to get data

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const data = require('./data');
 
-const averageCardLevel = (rarity) => {
-    return data
+const averageCardLevel = (matches, rarity) => {
+    let average = matches
         .reduce((a, m) => a.concat(m[5]), [])
-        .filter((c) => c[2] === 3)
-        .reduce((a, c, i, o) => a + c[1]/o.length, 0);
+        .filter((c) => c[2] === rarity)
+        .reduce((a, c, i, o) => a + c[1]/o.length, 0)
+        .toFixed(3);
+    return `Average level of cards of rarity: ${rarity} is ${average}`;
 };
 
-console.log(averageCardLevel(3));
+const cardFrequency = (matches) => {
+    let frequency = matches
+        .reduce((a, m) => a.concat(m[5]), [])
+        .reduce((a, c) => {
+            if (!a[c[0]]) {
+                a[c[0]] = 1;
+            } else {
+                ++a[c[0]];
+            }
+            return a;
+        }, {});
+    let prettyFrequency = Object.keys(frequency)
+        .sort()
+        .sort((a, b) => frequency[b] - frequency[a])
+        .map((c) => `${(c + '                ').substr(0,15)} ${frequency[c]}`);
+    return `${prettyFrequency.length} out of 70 cards found:\n${prettyFrequency.join('\n')}`;
+};
+
+console.log(averageCardLevel(data, 0));
+console.log(averageCardLevel(data, 1));
+console.log(averageCardLevel(data, 2));
+console.log(averageCardLevel(data, 3));
+console.log(cardFrequency(data));
