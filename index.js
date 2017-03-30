649ba737b7685453825f2bc2afba67f74258df54
Gabriel <pabombs@gmail.com>
2017-03-29 20:00:10 -0400

    added all matches and formatted data

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const data = require('./data');
 
-console.log(data.length);
+const averageCardLevel = (rarity) => {
+    return data
+        .reduce((a, m) => a.concat(m[5]), [])
+        .filter((c) => c[2] === 3)
+        .reduce((a, c, i, o) => a + c[1]/o.length, 0);
+};
+
+console.log(averageCardLevel(3));
