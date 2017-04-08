b5d19b6f1a109bf8f26b34fa7cafe5ec4b296ccd
Gabriel <pabombs@gmail.com>
2017-04-07 20:32:53 -0400

    refactor of calculation functions

--- index.js
+++ index.js
@@ -1,34 +1,75 @@
 const data = require('./data');
 
-const averageCardLevel = (matches, rarity) => {
-    let average = matches
+const mean = (nums) => {
+    return nums.reduce((a, n) => a + n, 0)/nums.length;
+};
+
+const median = (nums) => {
+    nums.sort((a, b) => b - a);
+    const medianIndex = nums.length/2;
+    if (medianIndex%2 === 0) {
+        return (nums[medianIndex - 1] + nums[medianIndex])/2;
+    } else {
+        return nums[Math.floor(medianIndex)];
+    }
+};
+
+const variance = (nums) => {
+    const average = mean(nums);
+    return mean(nums.map((n) => (average - n) ** 2));
+};
+
+const standardDeviation = (nums) => {
+    return Math.sqrt(variance(nums));
+};
+
+const getlevelsOfCardsOfRarity = (matches, rarity) => {
+    return matches
         .reduce((a, m) => a.concat(m[5]), [])
         .filter((c) => c[2] === rarity)
-        .reduce((a, c, i, o) => a + c[1]/o.length, 0)
-        .toFixed(3);
-    return `Average level of cards of rarity: ${rarity} is ${average}`;
+        .map((c) => c[1]);
 };
 
 const cardFrequency = (matches) => {
-    let frequency = matches
+    return matches
         .reduce((a, m) => a.concat(m[5]), [])
         .reduce((a, c) => {
-            if (!a[c[0]]) {
-                a[c[0]] = 1;
+            let index = a.findIndex((e) => e[0] === c[0]);
+            if (index < 0) {
+                a.push([c[0], 1]);
             } else {
-                ++a[c[0]];
+                ++a[index][1];
             }
             return a;
-        }, {});
-    let prettyFrequency = Object.keys(frequency)
-        .sort()
-        .sort((a, b) => frequency[b] - frequency[a])
-        .map((c) => `${(c + '                ').substr(0,15)} ${frequency[c]}`);
-    return `${prettyFrequency.length} out of 70 cards found:\n${prettyFrequency.join('\n')}`;
+        }, [])
+        .sort((a, b) => b[1] - a[1]);
 };
 
-console.log(averageCardLevel(data, 0));
-console.log(averageCardLevel(data, 1));
-console.log(averageCardLevel(data, 2));
-console.log(averageCardLevel(data, 3));
-console.log(cardFrequency(data));
+// card frequency
+const levels = cardFrequency(data).map((c) => c[1]);
+// some cards are missing, total number of cards is 70
+while(levels.length < 70) {
+    levels.push(0);
+}
+console.log(`
+    Card frequency:
+        sample mean: ${mean(levels).toFixed(5)}
+        sample median: ${median(levels).toFixed(5)}
+        sample variance: ${variance(levels).toFixed(5)}
+        sample standard deviation: ${standardDeviation(levels).toFixed(5)}
+`);
+
+console.log('===================================================')
+
+// card levels per rarity
+const r = [] && ['common', 'rare', 'epic', 'legendary'];
+for (var i = 0; i < r.length; i++) {
+    const levels = getlevelsOfCardsOfRarity(data, i);
+    console.log(`
+    Card level for rarity ${r[i]}:
+        sample mean: ${mean(levels).toFixed(5)}
+        sample median: ${median(levels).toFixed(5)}
+        sample variance: ${variance(levels).toFixed(5)}
+        sample standard deviation: ${standardDeviation(levels).toFixed(5)}
+    `);
+}
